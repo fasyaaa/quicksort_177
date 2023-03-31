c00e4180ed128fed2cbc7c50aceff5c1056ca5fe
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-03-31 12:27:41 +0700

    menambah comment

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int arr[20];
-int cmp_count = 0;
-int mov_count = 0;
+int arr[20];				// Array of integers to hild values
+int cmp_count = 0;			// number of comparasion
+int mov_count = 0;			// number of data movements
 int n;
