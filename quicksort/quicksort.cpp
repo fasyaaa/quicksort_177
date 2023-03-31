eaa44c8550e08be3ca545604566f431c19c9e8f9
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-03-31 12:33:38 +0700

    menambah bagian void input

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -5,3 +5,25 @@ int arr[20];				// Array of integers to hild values
 int cmp_count = 0;			// number of comparasion
 int mov_count = 0;			// number of data movements
 int n;
+
+void input() {
+	while (true) 
+	{
+		cout << "Masukkan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
